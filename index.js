ef2143c557d3e93fe0b2dd6d56027c64e79e68ed
Ksenya2991 <ksenya2991@gmail.com>
2022-12-17 18:20:52 +0500

    Adaptive menu added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const burger = document.querySelector('.burger');
-const list = document.querySelector('.list');
+// const burger = document.querySelector('.burger');
+// const list = document.querySelector('.list');
 
-function showList() {
-    burger.classList.add('active');
-}
+// function showList() {
+//     burger.classList.add('active');
+// }
 
-burger.addEventListener('click', () => showList());
+// burger.addEventListener('click', () => showList());
+
+let menuBtn = document.querySelector('.burger');
+let menu = document.querySelector('.list');
+
+menuBtn.addEventListener('click', function() {
+    menu.classList.toggle('active');
+})
